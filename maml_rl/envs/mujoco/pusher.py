Fix bug in pusher where the reward calculation is wrong due to flipped observation array

maml_rl/envs/mujoco/pusher.py
@@ -41,8 +41,8 @@ class PusherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def _get_obs(self):
         return np.concatenate([
             self.sim.data.qpos.flat[:3],
-            self.sim.data.qvel.flat,
             self.sim.data.geom_xpos[-6:-1, :2].flat,
+            self.sim.data.qvel.flat,
         ]).astype(np.float32).flatten()
 
 
@@ -75,6 +75,7 @@ class PusherTaskEnv(PusherEnv):
             reward = -5 * goal_dist
         else:
             reward = -5 * block_dist
+
         done = False
         infos = dict()
 

render.py
@@ -7,10 +7,9 @@ import logging
 from IPython import embed
 
 from maml_rl.metalearner import MetaLearner
-from maml_rl.policies import CategoricalMLPPolicy, NormalMLPPolicy
+from maml_rl.policies import CategoricalMLPPolicy, NormalMLPPolicy, IntrinsicReward
 from maml_rl.baseline import LinearFeatureBaseline
 from maml_rl.sampler import TrajectorySampler
-from maml_rl.reward import IntrinsicReward
 
 from tensorboardX import SummaryWriter
 
@@ -44,7 +43,7 @@ def main(args):
         state_dict = torch.load(args.policy_checkpoint)
         policy.load_state_dict(state_dict)
 
-    task = sampler.sample_tasks(num_tasks=1, seed=999)[0]
+    task = sampler.sample_tasks(num_tasks=5, seed=999)[0]
     sampler.reset_task(task)
     episodes = sampler.sample(policy)
 

test_vpg_simple.py
@@ -9,10 +9,9 @@ from torch import optim
 from IPython import embed
 
 from maml_rl.metalearner import MetaLearner
-from maml_rl.policies import CategoricalMLPPolicy, NormalMLPPolicy
+from maml_rl.policies import CategoricalMLPPolicy, NormalMLPPolicy, IntrinsicReward
 from maml_rl.baseline import LinearFeatureBaseline
 from maml_rl.sampler import BatchSampler
-from maml_rl.reward import IntrinsicReward
 from maml_rl.utils.torch_utils import (weighted_mean, detach_distribution,
                                        weighted_normalize)
 
@@ -92,7 +91,7 @@ def main(args):
         int(np.prod(sampler.envs.observation_space.shape)))
     optimizer = optim.SGD(policy.parameters(), lr=1e-2)
 
-    task = sampler.sample_tasks(num_tasks=1, seed=999)[0]
+    task = sampler.sample_tasks(num_tasks=5, seed=999)[0]
     for batch in range(args.num_batches):
         logger.debug('Processing batch {}'.format(batch))
 